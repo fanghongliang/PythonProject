e8513f8f39e015d4037adc288981af0800deb02e
fanghongliang <18292656094@163.com>
2020-09-16 10:53:52 +0800

    feat: 红图权限隔离

--- ginger/app/libs/scope.py
+++ ginger/app/libs/scope.py
@@ -17,10 +17,12 @@ class Scope:
 
 class UserScope(Scope):
     allow_api = ['v1.get_user']
+    allow_module = []
 
 
 class AdminScope(Scope):
-    allow_api = ['v1.super_get_user']
+    # allow_api = ['v1.super_get_user']
+    allow_module = ['v1.user']
 
     def __init__(self):
         self + UserScope()
@@ -28,6 +30,7 @@ class AdminScope(Scope):
 
 class SuperScope(Scope):
     allow_api = []
+    allow_module = []
 
     def __init__(self):
         self + UserScope() + AdminScope()
@@ -38,8 +41,13 @@ def is_in_scope(scope, endpoint):
     # globals
     # 反射
     # token 内部携带权限参数，直接判断权限
+    # v1.red_name + view_func
     scope = globals()[scope]()
+    splits = endpoint.split('+')
+    red_name = splits[0]
     if endpoint in scope.allow_api:
+        return True
+    if red_name in scope.allow_module:
         return True
     else:
         return False
