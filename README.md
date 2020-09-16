d5511f7436c7fa2c962044ffc2e701c3e568def2
fanghongliang <18292656094@163.com>
2020-09-16 11:11:40 +0800

    feat: 权限筛选实现

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
-## PythonProject
-python-flask项目
-------
+## 基于Python-Flask框架的后台restapi实现  
+* author: Fanghl  
+-----  
+后台使用mysql数据库，ORM使用sqlalchemy，进行了 token 的发放与验证,严格实现了用户接口访问权限功能。针对浏览器环境，处理了跨域请求。对前端来的请求进行了基本的增删改查登录等接口，并且已将本地项目部署在云服务器上。最终暴露对外的公网接口可以访问 160.13.4.75：5000/api/login (post),在login接口获得access_token， 将token放置在之后的请求头内即可访问余下的api。  
+[![22-B80342-4-A69-4e82-8-E81-56535-FFE0126.png](https://i.postimg.cc/MHHPNvh3/22-B80342-4-A69-4e82-8-E81-56535-FFE0126.png)](https://postimg.cc/gL9H6kc3)
+
 
 ### 技术点  
 
-> * flask  
-> * python3.7
+> * Flask  
+> * sqlalchemy
 > * mysql
+> * centos8(服务器环境)
+> * nginx 
 
-### How to run   
+### 架构   
+* app.js 是入口核心文件
+* /models 数据库表模型文件
+* /routes 存放我们所有的路由
+* /routes/core.js 在该文件内，express应用所有的路由，新添加的路由文件也应该放在 core 内导出，最后在app.js内只需要use('core.js')即可应用所有路由
+* /utils/log 该文件夹存储api调用日志，已天为单位，一个自然日的日志单独生成一个日志文件 
+* /utils 内存放项目配置文件，包括中间件、数据库连接配置等。
 
-* 激活虚拟环境  
-> $ env/Scripts/active
+### How to run 
 
+* install dependencies
+> pipenv install
 
+* serve with hot reload at localhost:5000
+> python jinger.py
 
------ 
+### possible problem  
+
+* 数据库配置文件（utils/sequelize.config）未更改为你自己的，或者直接使用我线上的数据库也 可以    
+* 依赖是否全部已安装  
+
+### supplement   
+
+至于服务器部署，可以去 http://fanghl.top/2020/06/09/server/#more 查阅更多
+
+-----
+
+> 本项目亮点在于用户权限管理的设计以及自定义红图-蓝图机制。如有这方面更好的意见，欢迎大家积极提出issue交流。  
+
+https://github.com/fanghongliang/PythonProject
+
--- ginger/app/libs/scope.py
+++ ginger/app/libs/scope.py
@@ -5,6 +5,8 @@ Created by Fanghl on 2020/9/15 16:31
 
 class Scope:
     allow_api = []
+    allow_module = []
+    forbidden = []
 
     def __add__(self, other):
         # 管理员合并普通用户权限
@@ -12,28 +14,34 @@ class Scope:
         self.allow_api = self.allow_api + other.allow_api
         # set 去重
         self.allow_api = list(set(self.allow_api))
+        # 红图级别权限相加
+        self.allow_module = self.allow_module + other.allow_module
+        self.allow_module = list(set(self.allow_module))
+        # 逆向筛选权限
+        self.forbidden = self.forbidden + other.forbidden
+        self.forbidden = list(set(self.forbidden))
         return self
 
 
 class UserScope(Scope):
-    allow_api = ['v1.get_user']
+    allow_api = ['v1.user+get_user']
     allow_module = []
+    forbidden = ['v1.user+super_get_user',
+                 'v1.user+super_delete_user']
+
+    def __init__(self):
+        self + AdminScope()
 
 
 class AdminScope(Scope):
-    # allow_api = ['v1.super_get_user']
+    # allow_api = ['v1.user+super_get_user',
+    #              'v1.user+super_delete_user']
     allow_module = ['v1.user']
 
     def __init__(self):
-        self + UserScope()
-
-
-class SuperScope(Scope):
-    allow_api = []
-    allow_module = []
-
-    def __init__(self):
-        self + UserScope() + AdminScope()
+        # 排除 筛选视图函数
+        # self + UserScope()
+        pass
 
 
 def is_in_scope(scope, endpoint):
@@ -45,6 +53,8 @@ def is_in_scope(scope, endpoint):
     scope = globals()[scope]()
     splits = endpoint.split('+')
     red_name = splits[0]
+    if endpoint in scope.forbidden:
+        return False
     if endpoint in scope.allow_api:
         return True
     if red_name in scope.allow_module:
