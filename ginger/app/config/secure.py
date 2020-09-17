79c33a51cb4bcf294cd09f72ca02f4ee5fd3cac0
fanghongliang <18292656094@163.com>
2020-09-17 10:55:16 +0800

    chore: 线上测试数据库配置

--- ginger/app/config/secure.py
+++ ginger/app/config/secure.py
@@ -3,7 +3,8 @@ Created by Fanghl on 2020/9/10 13:21
 """
 
 
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@127.0.0.1:3306/ginger'
+# SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@127.0.0.1:3306/ginger'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@106.13.4.75:3306/ginger'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_TEARDOWN = True
 
